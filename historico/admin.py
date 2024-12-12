f0c64df28691f88b34b731c2db67b39e6476f1d8
Marcelo Ferraz de Oliveira <mfoliveira@tce.sp.gov.br>
2024-12-12 10:43:13 -0300

    Adiciona o modelo ao admin

--- historico/admin.py
+++ historico/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Acao
 
-# Register your models here.
+@admin.register(Acao)
+class AcaoAdmin(admin.ModelAdmin):
+    list_display = ('papel', 'cotacao', 'pl', 'pvp', 'div_yield', 'roic', 'roe', 'data')
+    search_fields = ('papel',)
+    list_filter = ('data', 'papel')
