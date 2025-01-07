8c2e271d70f8137e1cfe4f268906cd3ed7695f9f
Marcelo Ferraz de Oliveira <32283105+Marcelo-Ferraz-de-Oliveira@users.noreply.github.com>
2025-01-07 12:31:58 -0300

    Remove STATICFILES_DIRS

--- historico_fundamentalista/settings.py
+++ historico_fundamentalista/settings.py
@@ -17,9 +17,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
